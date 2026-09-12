8fb53a5a95108f97060931cb60c574e82030a025
Simon Ukus <ukussimon2@gmail.com>
2026-09-12 15:06:51 +0200

    Add custom logo
    Signed-off-by: Simon Ukus <ukussimon2@gmail.com>

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -1,22 +1,18 @@
-import type { ComponentPropsWithRef } from "react"
+"use client"
+
+import { type ComponentPropsWithRef } from "react"
 
 export function Logo(props: ComponentPropsWithRef<"svg">) {
   return (
     <svg
-      className="size-5"
-      fill="none"
-      viewBox="0 0 60 45"
+      className="size-10 fill-black stroke-black dark:fill-neutral-300 dark:stroke-neutral-300"
       xmlns="http://www.w3.org/2000/svg"
-      aria-label="Better Auth UI"
-      role="img"
+      viewBox="0 75 500 350"
       {...props}
     >
-      <path
-        fill="currentColor"
-        clipRule="evenodd"
-        d="M0 0H15V45H0V0ZM45 0H60V45H45V0ZM20 0H40V15H20V0ZM20 30H40V45H20V30Z"
-        fillRule="evenodd"
-      />
+      <polygon points="0.833 78.913 0.547 201.887 200.666 408.64 220.956 417.13 263.605 417.13 263.788 345.775"></polygon>
+      <polygon strokeWidth={1} points="160.735 77.053 160.449 200.027 360.568 406.78 380.858 415.27 423.507 415.27 423.69 343.915"></polygon>
+      <path d="M 343.657 151.883 L 343.99 230.101 L 423.438 311.597 C 426.496 182.106 409.07 210.573 496.931 153.849 L 499.453 83.786 L 414.156 82.212 L 343.657 151.883 Z"></path>
     </svg>
   )
 }
