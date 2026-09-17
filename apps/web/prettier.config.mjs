ec341a1aec1b479f1f2bf932911f3dddd5e75992
Simon Ukus <ukussimon2@gmail.com>
2026-09-17 18:14:23 +0200

    Use single prettier config
    Signed-off-by: Simon Ukus <ukussimon2@gmail.com>

--- apps/web/prettier.config.mjs
+++ apps/web/prettier.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  semi: false,
+  semi: true,
   tailwindFunctions: ["cn", "cva"],
   tailwindStylesheet: "./app/globals.css",
 };
